21659f3d1840246fca2ba1587462adc6b07a42fa
yeonjin seong <syj3405@naver.com>
2021-12-04 11:38:30 +0900

    프래그먼트 코틀린 클래스 생성   - xml을 끌어다 연결   - 액티비티 : setContentView() 대응

--- app/src/main/java/com/neppplus/fragment_20211204/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20211204/fragments/FirstFragment.kt
@@ -1,6 +1,21 @@
 package com.neppplus.fragment_20211204.fragments
 
-class FirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.fragment_20211204.R
 
+
+class FirstFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first,container,false)
+    }
 
 }
