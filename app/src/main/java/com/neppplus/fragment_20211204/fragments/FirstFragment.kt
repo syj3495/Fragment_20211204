430642f41d4f7fd7569782ca28b96a262d05c5f4
yeonjin seong <syj3405@naver.com>
2021-12-04 11:43:23 +0900

    프래그먼트 코틀린 클래스 생성   - xml을 끌어다 연결   - 액티비티 : setContentView() 대응

--- app/src/main/java/com/neppplus/fragment_20211204/fragments/FirstFragment.kt
+++ app/src/main/java/com/neppplus/fragment_20211204/fragments/FirstFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.neppplus.fragment_20211204.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 
 class FirstFragment : Fragment(){
+
+//    xml을 연결해주는 역할만 담당
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +20,20 @@ class FirstFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first,container,false)
+    }
+
+//    동작 관련 코드 담당
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnToast.setOnClickListener {
+
+            Toast.makeText(requireContext(), "프래그먼트에서 토스트 띄우기", Toast.LENGTH_SHORT).show()
+
+
+
+        }
     }
 
 }
